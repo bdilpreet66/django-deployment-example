932f9b0f70688b0a0273f16d64a1b1b7ffe3966b
bdilpreet66 <42580896+bdilpreet66@users.noreply.github.com>
2018-11-15 21:37:13 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'bdilpreet66.pythonanywhere.com',
 ]
 
 ROOT_URLCONF = 'learning_templates.urls'
